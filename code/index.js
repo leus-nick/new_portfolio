cb78a96ec7144a04b36fe44d16f985c73a2c8abc
leus-nick <nickaccreg@gmail.com>
2021-08-04 14:17:58 +0300

    Styles for skills section and basic logic for scroll observation

--- code/index.js
+++ code/index.js
@@ -1,1 +1,30 @@
-console.log('ALL WORKING FINE!');
+// FILL HEADER ON SCROLL
+
+window.addEventListener('scroll', function () {
+  if (pageYOffset >= 50) {
+    document.querySelector('nav').classList.add('filled');
+  } else {
+    document.querySelector('nav').classList.remove('filled');
+  }
+});
+
+// OBSERVER TO START THE ANIMATION WHEN THE OBJECT IS IN THE VIEWPORT
+
+const observer = new IntersectionObserver((entries) => {
+  console.log(entries);
+  entries.forEach((entry) => {
+    console.log(entry.target);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('circle__animation');
+    }
+  });
+});
+
+const skillList = document.querySelectorAll('.circle');
+console.log(skillList);
+
+skillList.forEach((skill) => {
+  observer.observe(skill);
+});
+
+//
